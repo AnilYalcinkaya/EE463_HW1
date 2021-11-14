85d23c81a7315012cadccf3b151771aaea2ad72d
Gktut <54786523+Gktut@users.noreply.github.com>
2021-11-14 22:49:22 +0300

    Add files via upload

---
+++ part2/parte.m
@@ -0,0 +1,22 @@
+clear
+clc
+close all
+
+out = sim('partb_e_v1.slx')
+
+time = out.tout;
+input_voltage = out.input_voltage.data;
+input_current = out.input_current.data;
+output_voltage = out.output_voltage.data;
+
+plot(time,input_voltage,'LineWidth',2)
+hold on
+plot(time,input_current,'LineWidth',2)
+plot(time,output_voltage,'LineWidth',2)
+grid minor
+legend('Input Voltage','Input Current','Output Voltage')
+xlabel('Time(Seconds)')
+ylabel('Voltage(V) and Ampere(A)')
+title('Input voltage, Input current and Output voltage vs time')
+xlim([0.7999 0.841])
+ylim([-500 800])
